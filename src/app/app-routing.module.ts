ca04a5d65a5acc0fc10defe59c983a7c4565a995
David Maduabuchi <davidmaduabuchi62@gmail.com>
2024-01-30 00:52:31 +0100

    lazy load added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,46 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AuthGuard } from './auth/auth.guard';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { CartComponent } from "./pages/cart/cart.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: "home",
     component: HomeComponent,
+    loadChildren: () =>
+    import("./lazy-loading/lazy-loading.module").then(
+      (m) => m.LazyLoadingModule
+    ),
   },
   {
-    path: 'cart',
+    path: "cart",
     component: CartComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
 
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent,
   },
 
   {
-    path: 'register',
+    path: "register",
     component: RegisterComponent,
   },
-  
-
 
   // The path below sets the home compoomemt to the default path
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
